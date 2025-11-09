0708106f77f2a21b6d844ba32a164bf8fbdb0750
Andrej Gruevski <andrejgruevski1@gmail.com>
2025-11-09 23:59:10 +0100

    Auditory exercise 4 (new exercise for objects)

--- Aud4/index.js
+++ Aud4/index.js
@@ -18,6 +18,7 @@ const person2 = {
     }
 }
 
+// Задача 1
 // Напишете JavaScript програма која ќе ги прикаже сите атрибути
 // на даден објект
 for (let key in person1 ) {
@@ -26,4 +27,52 @@ for (let key in person1 ) {
 let keys = Object.keys(person2);
 keys.forEach(key =>console.log(person2[key]));
 
+// Задача 2
+// Напишете JavaScript програма која ќе гo избрише атрибутот
+// rollno од следниот објект
+let student = {
+    name: "Viktor",
+    sclass: "PIT",
+    rollno: "12"
+}
+console.log(student);
+delete student.rollno;
+console.log(student);
 
+
+// Задача 3
+// Напишете JavaScript програма која во конзола ќе ги испечати
+// податоците за следните објекти (книга, автор и статус)
+let library =[
+    {
+        author: "Bill Gates",
+        title: "The Road Ahead",
+        readingStatus: true
+    },
+    {
+        author: "Steve Jobs",
+        title: "Walter Isaacson",
+        readingStatus: true
+    },
+    {
+        author: "Suzanne Collins",
+        title: "Mockingjay: The final Book of The Hunger Games",
+        readingStatus: false
+    }
+]
+for (let i =0; i<library.length; i++) {
+    console.log("Book: " + (i + 1) + ":");
+    console.log(("Author: " + library[i].author));
+    console.log(("Title: " + library[i].title));
+    console.log(("Reading Status: " + library[i].author));
+    console.log(("----------------------"));
+}
+
+// Задача 4
+// Напишете JavaScript функција која ќе трансформира објект во
+// листа од `[key,value]` парови
+function objectToList(obj){
+    return Object.entries(obj);
+}
+let colors = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+console.log(objectToList(colors));
